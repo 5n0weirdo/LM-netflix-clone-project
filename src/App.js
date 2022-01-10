ccb6c98734f551dd1b364a73c1663b2a434adabd
5n0weirdo <snoweirdo@protonmail.com>
2022-01-10 18:43:29 +0200

    Change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import React from "react";
 import Row from "./Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>Developer</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
