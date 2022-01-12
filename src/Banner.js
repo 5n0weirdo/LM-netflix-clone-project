973fb1dd34efebb0171f189cf4b8041952b47177
5n0weirdo <snoweirdo@protonmail.com>
2022-01-12 14:41:13 +0200

    Added gradient style to banner

--- src/Banner.js
+++ src/Banner.js
@@ -50,6 +50,8 @@ function Banner() {
         {/* desc */}
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+
+      <div className="banner_fadeBottom" />
     </header>
   );
 }
